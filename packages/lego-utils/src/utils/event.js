98293a75c9e5df9d8cd771334433a727ecdf565f
小宫同学 <gong1782523482@163.com>
2022-07-22 01:10:27 +0800

    feat: git hooks

---
+++ packages/lego-utils/src/utils/event.js
@@ -0,0 +1,34 @@
+class EventEmitter {
+  constructor() {
+    this.cache = {}
+  }
+
+    /**
+     * 订阅
+     * @param {string} eventname
+     * @param {function} fn
+     */
+    on = (name, fn) => {
+      this.cache[name] = fn
+    };
+
+    /**
+     * 发布
+     * @param {string} eventname
+     */
+    emit = (name, ...args) => {
+      if (this.cache[name]) this.cache[name](...args)
+    };
+
+    off = (name, fn) => {
+      const tasks = this.cache[name]
+      if (tasks) {
+        const index = tasks.findIndex((f) => f === fn || f.callback === fn)
+        if (index >= 0) {
+          tasks.splice(index, 1)
+        }
+      }
+    };
+}
+
+export default new EventEmitter()
