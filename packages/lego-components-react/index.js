a15e740c8688551ee7b7907ae47bd0acf757e9af
小宫同学 <gong1782523482@163.com>
2022-07-25 11:07:50 +0800

    fix: bugfix

--- packages/lego-components-react/index.js
+++ packages/lego-components-react/index.js
@@ -3,7 +3,7 @@ import './style.scss'
 import Button from './src/components/Button/index'
 import Dialog from './src/components/Dialog/index'
 import Image from './src/components/Image/index'
-import NoticeBar from './src/components/NoticeBar'
+import NoticeBar from './src/components/NoticeBar/index'
 import getSchma from './src/util/getSchema'
 
 // 组合组件物料信息
