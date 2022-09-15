ecaaadc900969dfb41873faa428e5d211dd28c42
gong <1782523482@163.com>
2022-09-15 17:37:01 +0800

    feat: js库增加文档

--- packages/poor-utils/src/utils/throttle.js
+++ packages/poor-utils/src/utils/throttle.js
@@ -1,7 +1,7 @@
 /**
  * 节流--时间戳版本
  * @param {function} func 要处理的函数
- * @param {number} 截流时间
+ * @param {number} delay 截流时间
  * @returns a handle after function
  */
 const throttle = (func, delay = 20) => {
